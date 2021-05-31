62d34a12829654be23a5685ec9205aa49122335d
pareshrane1998 <pareshr32@gmail.com>
2021-05-31 14:34:00 +0530

    logic for hamburger menu added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-/* Author: 
+// logic for hamburger menu
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
-*/
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active")
+});
 
 
 
